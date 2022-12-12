197be6d1436810a1300c247a9d96aadc05efad13
ytpit20218 <80588847+ytpit20218@users.noreply.github.com>
2022-12-12 22:16:01 +0700

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -1,69 +1,43 @@
-#! /usr/bin/python3
-
-banner = r'''
-#########################################################################
-#      ____            _           _   __  __                           #
-#     |  _ \ _ __ ___ (_) ___  ___| |_|  \/  | ___   ___  ___  ___      #
-#     | |_) | '__/ _ \| |/ _ \/ __| __| |\/| |/ _ \ / _ \/ __|/ _ \     #
-#     |  __/| | | (_) | |  __/ (__| |_| |  | | (_) | (_) \__ \  __/     #
-#     |_|   |_|  \___// |\___|\___|\__|_|  |_|\___/ \___/|___/\___|     #
-#                   |__/                                                #
-#                                  >> https://github.com/benmoose39     #
-#########################################################################
-'''
-
+from flask import Flask, request
 import requests
-import os
 import sys
 
-windows = False
-if 'win' in sys.platform:
-    windows = True
+HOST = sys.argv[1]
+PORT = 9001
 
-def grab(url):
-    response = requests.get(url, timeout=15).text
-    if '.m3u8' not in response:
-        #response = requests.get(url).text
-        if '.m3u8' not in response:
-            if windows:
-                print('https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u')
-                return
-            #os.system(f'wget {url} -O temp.txt')
-            os.system(f'curl "{url}" > temp.txt')
-            response = ''.join(open('temp.txt').readlines())
-            if '.m3u8' not in response:
-                print('https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u')
-                return
-    end = response.find('.m3u8') + 5
-    tuner = 100
-    while True:
-        if 'https://' in response[end-tuner : end]:
-            link = response[end-tuner : end]
-            start = link.find('https://')
-            end = link.find('.m3u8') + 5
-            break
-        else:
-            tuner += 5
-    print(f"{link[start : end]}")
+app = Flask(__name__)
 
-print('#EXTM3U x-tvg-url="https://github.com/botallen/epg/releases/download/latest/epg.xml"')
-print(banner)
-#s = requests.Session()
-with open('../youtube_channel_info.txt') as f:
-    for line in f:
-        line = line.strip()
-        if not line or line.startswith('~~'):
-            continue
-        if not line.startswith('https:'):
-            line = line.split('|')
-            ch_name = line[0].strip()
-            grp_title = line[1].strip().title()
-            tvg_logo = line[2].strip()
-            tvg_id = line[3].strip()
-            print(f'\n#EXTINF:-1 group-title="{grp_title}" tvg-logo="{tvg_logo}" tvg-id="{tvg_id}", {ch_name}')
-        else:
-            grab(line)
-            
-if 'temp.txt' in os.listdir():
-    os.system('rm temp.txt')
-    os.system('rm watch*')
+@app.route(f'/playlist.m3u')
+def playlistgenerator():
+    playlist = '#EXTM3U\n'
+    with open('streams.txt', 'r') as streams:
+        for stream in streams:
+            stream = stream.strip()
+            if not stream:
+                continue
+            playlist += f'#EXTINF:-1 group-title="twitch", {stream}\n'
+            playlist += f'http://{HOST}:{PORT}/twitch?streamer={stream}\n'
+
+    return playlist
+
+
+@app.route(f'/twitch')
+def getm3u():
+    streamer = request.args.get('streamer')
+    url = 'https://pwn.sh/tools/streamapi.py?url=twitch.tv/'
+    try:
+        response = requests.get(f'{url}{streamer}').json()
+        links = response['urls']
+        qualities = {int(key.replace('p','')) : key for key in links.keys() if key != 'audio_only'}
+
+        m3u = ''
+        for quality in sorted(qualities.keys(), reverse=True):
+            m3u = m3u + links[qualities[quality]] + '\n'
+                
+    except:
+        m3u = 'https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u'
+    
+    return f'#EXTM3U\n#EXT-X-INDEPENDENT-SEGMENTS\n#EXT-X-STREAM-INF:BANDWIDTH=290288\n{m3u}'
+
+if __name__ == '__main__':
+    app.run(host=HOST, port=PORT)
